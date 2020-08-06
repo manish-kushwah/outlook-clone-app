8fb5bc88ad671ca2119dfbab12983e785a9c846b
manish-kushwah <manishkushwah758@gmail.com>
2020-08-06 08:51:02 +0530

    Webpack & Babel configured

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: { loader: "source-map-loader" },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        use: { loader: "json-loader" },
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        exclude: /node_modules/,
+        use: { loader: "url-loader" },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
